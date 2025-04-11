85d60759c5e3153fa95adc35f3d4f1fdc308838f
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-04-11 16:21:29 +0100

    Tweaks to MPV

--- provisioning/mpv.sh
+++ provisioning/mpv.sh
@@ -2,7 +2,4 @@
 
 cd "$BASE"/mediaserver
 
-mpv -v --no-video --keep-open=yes --input-ipc-server="$MPV_SOCKET" --idle=yes
-
-
-
+mpv -v --no-video --keep-open=yes --input-ipc-server="$MPV_SOCKET" --idle=yes --display-tags-clr --msg-level=cplayer=error --no-terminal
