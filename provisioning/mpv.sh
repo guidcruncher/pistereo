fc8abd126f28aa74fd4160a41a363cbe17f5326a
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-04-14 12:43:03 +0100

    Equaliser ASound settings for Alsa

--- provisioning/mpv.sh
+++ provisioning/mpv.sh
@@ -2,4 +2,4 @@
 
 cd "$BASE"/mediaserver
 
-mpv -v --no-video --keep-open=yes --input-ipc-server="$MPV_SOCKET" --idle=yes --display-tags-clr --msg-level=cplayer=error --no-terminal
+mpv -v --alsa-mixer-device=equal --no-video --keep-open=yes --input-ipc-server="$MPV_SOCKET" --idle=yes --display-tags-clr --msg-level=cplayer=error --no-terminal
