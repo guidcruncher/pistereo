eeca7cea8f7ae570be3794947517b2af89927938
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-04-16 11:18:53 +0100

    Fixs

--- provisioning/librespot.sh
+++ provisioning/librespot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# $ALSA_MIXER_DEVICE
+envsubst <  "$BASE"/mediaserver/default-mediaserver.config > "$BASE"/mediaserver/config/config.yml
+
 cd "$BASE"/mediaserver
 ./go-librespot --config_dir "$BASE"/mediaserver/config/
-
-
