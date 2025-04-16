0b375de622c2b5b7ba85f9c5bf26987823a34e03
John Crocker <182365840+guidcruncher@users.noreply.github.com>
2025-04-16 13:15:11 +0100

    Tidied

--- provisioning/mpv.sh
+++ provisioning/mpv.sh
@@ -2,4 +2,4 @@
 
 cd "$BASE"/mediaserver
 
-mpv -v --alsa-mixer-device="$ALSA_DEVICE" --no-video --keep-open=yes --input-ipc-server="$MPV_SOCKET" --idle=yes --display-tags-clr --msg-level=cplayer=error --no-terminal
+mpv -v --alsa-mixer-device="$ALSA_DEVICE" --no-video --keep-open=yes --input-ipc-server="$MPV_SOCKET" --idle=yes --display-tags-clr --msg-level=cplayer=error
